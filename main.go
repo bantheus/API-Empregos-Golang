31f3914e1030a932fb0ecbec7788be39a6790aec
bantheus <matheuschmidt.b@gmail.com>
2023-11-27 14:38:53 -0300

    ✨ Feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/bantheus/API-Empregos-Golang/router"
+import (
+	"github.com/bantheus/API-Empregos-Golang/config"
+	"github.com/bantheus/API-Empregos-Golang/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
